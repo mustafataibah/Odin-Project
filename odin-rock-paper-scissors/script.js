13a9e652812e11dbebb7a17b7eacbf8b4872510c
Mustafa <mustafataibah@gmail.com>
2023-05-31 04:47:54 -0400

    Simple Rock Paper Scisors Project

---
+++ odin-rock-paper-scissors/script.js
@@ -0,0 +1,14 @@
+function getComputerChoice() {
+    const availableChoices = ['rock', 'paper', 'scissors']
+    const randomIndex = Math.floor(Math.random() * availableChoices.length)
+
+    const choice = availableChoices[randomIndex];
+
+    return choice
+}
+
+
+function playRound(playerSelection, computerSelection) {
+
+
+}   
