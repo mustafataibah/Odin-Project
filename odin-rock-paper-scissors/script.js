812e452594686fde51215f0d28582acc45980c83
Mustafa <mustafataibah@gmail.com>
2023-05-31 22:54:39 -0400

    copilot

--- odin-rock-paper-scissors/script.js
+++ odin-rock-paper-scissors/script.js
@@ -8,7 +8,31 @@ function getComputerChoice() {
 }
 
 
-function playRound(playerSelection, computerSelection) {
-
-
+function playRound(playerSelection) {
+    const computerSelection = getComputerChoice();
+  switch (playerSelection) {
+    case "rock":
+        if (computerSelection === "rock") {
+            return "It's a tie!"}
+        else if (computerSelection === "paper") {
+            return "You lose! Paper beats rock."}
+        else if (computerSelection === "scissors") {
+            return "You win! Rock beats scissors."}
+    case "paper":
+        if (computerSelection === "rock") { 
+            return "You win! Paper beats rock."}    
+        else if (computerSelection === "paper") {
+            return "It's a tie!"}
+        else if (computerSelection === "scissors") {
+            return "You lose! Scissors beats paper."}
+    case "scissors":    
+        if (computerSelection === "rock") { 
+            return "You lose! Rock beats scissors."}
+        else if (computerSelection === "paper") {
+            return "You win! Scissors beats paper."}
+        else if (computerSelection === "scissors") {
+            return "It's a tie!"}
+    default:
+        break;
+  }
 }   
