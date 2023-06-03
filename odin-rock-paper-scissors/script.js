e56d0a414ce0ae15a2421c2a81954dc9c499d657
Mustafa <mustafataibah@gmail.com>
2023-06-03 00:56:29 -0400

    Add an interactive background that moves w cursos

--- odin-rock-paper-scissors/script.js
+++ odin-rock-paper-scissors/script.js
@@ -7,40 +7,59 @@ function getComputerChoice() {
     return choice
 }
 
+const el = document.querySelector("#body");
+const bg = document.querySelector("#bg");
+
+window.addEventListener("mousemove", (e) => {
+    const xPercent = (e.clientX / window.innerWidth - 0.5) * 10;  
+    const yPercent = (e.clientY / window.innerHeight - 0.5) * 10;
+    bg.style.transform = `translate(${xPercent}%, ${yPercent}%)`;
+});
+
+let computerCounter = 0;
+let playerCounter = 0;
+let gamesPlayed = 0;
+
 function playRound(playerSelection) {
-    let computerCounter = 0;
-    let playerCounter = 0;
     const computerSelection = getComputerChoice(); 
-  switch (playerSelection) {
-    case "rock":
-        if (computerSelection === "rock") {
-            break;}
-        else if (computerSelection === "paper") {
-            computerCounter = computerCounter + 1;
-            break;}
-        else if (computerSelection === "scissors") {
-            playerCounter = playerCounter + 1;
-            break;}
-    case "paper":
-        if (computerSelection === "rock") { 
-            playerCounter = playerCounter + 1;}
-        else if (computerSelection === "paper") {
-            break;}
-        else if (computerSelection === "scissors") {
-            computerCounter = computerCounter + 1;
-            break;}
-    case "scissors":    
-        if (computerSelection === "rock") { 
-            computerCounter = computerCounter + 1;
-            break;}
-        else if (computerSelection === "paper") {
-            playerCounter = playerCounter + 1;
-            break;}
-        else if (computerSelection === "scissors") {
-            break;}
-    default:
-       break;
-  }
-  
-  console.log(`Player: ${playerCounter} Computer: ${computerCounter}`);
-}
+    
+    switch (playerSelection) {
+        case "rock":
+            if (computerSelection === "rock") {
+                break;}
+            else if (computerSelection === "paper") {
+                computerCounter = computerCounter + 1;
+                break;}
+            else if (computerSelection === "scissors") {
+                playerCounter = playerCounter + 1;
+                break;}
+        case "paper":
+            if (computerSelection === "rock") { 
+                playerCounter = playerCounter + 1;}
+            else if (computerSelection === "paper") {
+                break;}
+            else if (computerSelection === "scissors") {
+                computerCounter = computerCounter + 1;
+                break;}case "scissors":    
+                if (computerSelection === "rock") { 
+                    computerCounter = computerCounter + 1;
+                    break;}
+                else if (computerSelection === "paper") {
+                    playerCounter = playerCounter + 1;
+                    break;}
+                else if (computerSelection === "scissors") {
+                    break;}
+            default:
+                break;
+        }
+    
+        document.getElementById('computer').innerHTML = computerCounter;
+        document.getElementById('you').innerHTML = playerCounter;
+        console.log(`Player: ${playerCounter} Computer: ${computerCounter}`);
+        if (computerCounter === 5 || playerCounter === 5) {
+            playerCounter = 0;
+            computerCounter = 0;
+            gamesPlayed = gamesPlayed + 1;
+            console.log("Game Over");
+        }
+    }
