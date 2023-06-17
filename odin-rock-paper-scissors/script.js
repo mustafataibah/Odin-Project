8c5bd3da90a1f9df6b122ba87f3e9b6fb3d128d2
Mustafa <mustafataibah@gmail.com>
2023-06-17 17:00:40 -0400

    updated playRound to match playerSelection

--- odin-rock-paper-scissors/script.js
+++ odin-rock-paper-scissors/script.js
@@ -1,88 +1,28 @@
-function getComputerChoice() {
-  const availableChoices = ["rock", "paper", "scissors"];
-  const randomIndex = Math.floor(Math.random() * availableChoices.length);
-
-  const choice = availableChoices[randomIndex];
-
-  return choice;
-}
-
-const el = document.querySelector("#body");
-const bg = document.querySelector("#bg");
-
 let computerCounter = 0;
 let playerCounter = 0;
 let gamesPlayed = 0;
-
-function playRound(playerSelection) {
-  const computerSelection = getComputerChoice();
-
-  switch (playerSelection) {
-    case "rock":
-      if (computerSelection === "rock") {
-        break;
-      } else if (computerSelection === "paper") {
-        computerCounter = computerCounter + 1;
-        break;
-      } else if (computerSelection === "scissors") {
-        playerCounter = playerCounter + 1;
-        break;
-      }
-    case "paper":
-      if (computerSelection === "rock") {
-        playerCounter = playerCounter + 1;
-        break;
-      } else if (computerSelection === "paper") {
-        break;
-      } else if (computerSelection === "scissors") {
-        computerCounter = computerCounter + 1;
-        break;
-      }
-    case "scissors":
-      if (computerSelection === "rock") {
-        computerCounter = computerCounter + 1;
-        break;
-      } else if (computerSelection === "paper") {
-        playerCounter = playerCounter + 1;
-        break;
-      } else if (computerSelection === "scissors") {
-        break;
-      }
-    default:
-      break;
-  }
-
-  document.getElementById("computer").innerHTML = computerCounter;
-  document.getElementById("you").innerHTML = playerCounter;
-  console.log(`Player: ${playerSelection} Computer: ${computerSelection}`);
-  console.log(`Player: ${playerCounter} Computer: ${computerCounter}`);
-  if (computerCounter === 5 || playerCounter === 5) {
-    playerCounter = 0;
-    computerCounter = 0;
-    gamesPlayed = gamesPlayed + 1;
-    console.log("Game Over");
-  }
-}
 
 function displayText() {
   var text = document.getElementById("textField");
   text.style.display = "block";
 }
 
-var counter = 1;
+let counter = 1;
+let playerSelection = 1;
+// 1 = rock, 2 = paper, 3 = scissor
 function frontClickChange() {
   if (counter == 0) {
     document.getElementById("clickChange").src = "images/rock.png";
     counter++;
-    console.log(counter);
+    playerSelection = counter;
   } else if (counter == 1) {
     document.getElementById("clickChange").src = "images/paper.png";
     counter++;
-    console.log(counter);
+    playerSelection = counter;
   } else if (counter == 2) {
     document.getElementById("clickChange").src = "images/scissor.png";
     counter = 0;
-    console.log(counter);
+    playerSelection = counter;
   }
 }
 
@@ -90,14 +30,62 @@ function backClickChange() {
   if (counter == 0) {
     document.getElementById("clickChange").src = "images/paper.png";
     counter = 2;
-    console.log(counter);
+    playerSelection = counter;
   } else if (counter == 2) {
     document.getElementById("clickChange").src = "images/rock.png";
     counter--;
-    console.log(counter);
+    playerSelection = counter;
   } else if (counter == 1) {
     document.getElementById("clickChange").src = "images/scissor.png";
     counter--;
-    console.log(counter);
+    playerSelection = counter;
+  }
+}
+
+function getComputerChoice() {
+  const availableChoices = [1, 2, 0];
+  const randomIndex = Math.floor(Math.random() * availableChoices.length);
+
+  const choice = availableChoices[randomIndex];
+
+  return choice;
+}
+
+function playRound(playerSelection) {
+  const computerSelection = getComputerChoice();
+
+  switch (playerSelection) {
+    case 1:
+      if (computerSelection === 1) {
+        break;
+      } else if (computerSelection === 2) {
+        computerCounter = computerCounter + 1;
+        break;
+      } else if (computerSelection === 0) {
+        playerCounter = playerCounter + 1;
+        break;
+      }
+    case 2:
+      if (computerSelection === 1) {
+        playerCounter = playerCounter + 1;
+        break;
+      } else if (computerSelection === 2) {
+        break;
+      } else if (computerSelection === 0) {
+        computerCounter = computerCounter + 1;
+        break;
+      }
+    case 0:
+      if (computerSelection === 1) {
+        computerCounter = computerCounter + 1;
+        break;
+      } else if (computerSelection === 2) {
+        playerCounter = playerCounter + 1;
+        break;
+      } else if (computerSelection === 0) {
+        break;
+      }
+    default:
+      break;
   }
 }
