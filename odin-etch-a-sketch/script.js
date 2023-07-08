15bd7259a993b49b25fc1dfc137bd0b3efe385bb
Mustafa <mustafataibah@gmail.com>
2023-07-07 22:07:11 -0400

    Fully functional etch a sketch game with coloris library as a color picker

--- odin-etch-a-sketch/script.js
+++ odin-etch-a-sketch/script.js
@@ -1,19 +1,28 @@
-window.addEventListener("load", setDefaultGrid);
-document.getElementById("clear").addEventListener("click", clearGrid);
+let currentColor = "#000000";
 
-function setDefaultGrid() {
-  setGridSize(16);
-  fillGrid(16);
-}
+window.addEventListener("load", () => {
+  let gridSize = document.getElementById("gridSize").value;
+  setGridSize(gridSize);
+  fillGrid(gridSize);
+});
+
+document.getElementById("clear").addEventListener("click", clearGrid);
+document.getElementById("erase").addEventListener("click", setErase, false);
+document.getElementById("colorPicker").addEventListener("click", openColorPicker);
+document.getElementById("gridSize").addEventListener("input", updateGridSize, false);
 
 function setGridSize(size) {
   let grid = document.getElementById("container");
   grid.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+  grid.style.gridAutoRows = `${600 / size}px`;
+  grid.style.gridAutoColumns = `${600 / size}px`;
 }
 
 function fillGrid(size) {
   for (let i = 0; i < size * size; i++) {
     let gridCell = document.createElement("div");
+    gridCell.style.width = `${600 / size}px`;
+    gridCell.style.height = `${600 / size}px`;
     gridCell.classList = "cell";
     gridCell.addEventListener("mouseover", changeColor);
     document.getElementById("container").appendChild(gridCell);
@@ -21,7 +30,7 @@ function fillGrid(size) {
 }
 
 function changeColor(e) {
-  e.target.style.backgroundColor = "black";
+  e.target.style.backgroundColor = currentColor;
 }
 
 function clearGrid() {
@@ -31,5 +40,25 @@ function clearGrid() {
     document.getElementById("container").removeChild(element);
   });
 
-  setDefaultGrid();
+  let gridSize = document.getElementById("gridSize").value;
+  setGridSize(gridSize);
+  fillGrid(gridSize);
+}
+
+function openColorPicker() {
+  let colorPicker = new Coloris("#colorPicker");
+  colorPicker.on("color:change", function (color) {
+    currentColor = color.hexString;
+  });
+}
+
+function setErase() {
+  currentColor = "#ffffff";
+}
+
+function updateGridSize() {
+  let gridSize = document.getElementById("gridSize").value;
+  document.getElementById("gridSizeValue").textContent = gridSize;
+  document.getElementById("gridSizeValue2").textContent = gridSize;
+  clearGrid();
 }
